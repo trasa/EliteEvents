79a5daf5dfae15736811b793d02fd0574868d9c0
Tony Rasa <trasa@meancat.com>
2026-01-23 16:47:00 -0800

    url format borked

--- EliteEvents.Visitors/Program.cs
+++ EliteEvents.Visitors/Program.cs
@@ -39,7 +39,7 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(_ =>
     if (File.Exists(passwordFile))
     {
         var password = File.ReadAllText(passwordFile).Trim();
-        config = config.Replace("default:", $"default:{password}");
+        config += ",password=" + password;
     }
     Console.WriteLine($"Config: {config}");
     var options = ConfigurationOptions.Parse(config);
